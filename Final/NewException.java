b66c28ec625330ed9fd0a766b9e396b8f0a1ba44
RainsWeep <49872694+RainsWeep@users.noreply.github.com>
2019-11-27 23:05:30 +0800

    Add files via upload

---
+++ Final/NewException.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class NewException extends Exception{
+	public NewException(){
+		
+ 	}
+	public NewException(String str){ 
+            super(str);
+ 	}
+}
---
+++ Final/father.java
@@ -0,0 +1,17 @@
+package Final;
+
+public class father {
+		String 	 Num;
+		String   Name;
+		String   Sex;
+	String getNum() {
+		    return Num;
+		  }
+	String getName() {
+		    return Name;
+		  }
+	String getSex() {
+		    return Sex;
+		  }
+	  
+}
